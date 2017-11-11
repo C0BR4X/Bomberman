d04b976258bdfc3cf52ae981c363f4072363b12a
Julien Del Don <julien.deldon@gmail.com>
2017-11-11 17:23:36 +0100

    still not as i'd like

--- Bomberman - Starter/Assets/Scripts/Bomb.cs
+++ Bomberman - Starter/Assets/Scripts/Bomb.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
@@ -15,26 +16,25 @@ public class Bomb : MonoBehaviour
 		 * 								- secondly: the delay before it gets called.
 		 * In this case, you we to make the bomb explode in three seconds, so we call Explode()
 		 */
-		Invoke("Explode", 5f);	
+		Invoke("Explode", 3f);	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Explode();
 	}
-
-	private void Explode()
-	{
-		/*
-		 * 1. Spawns an explosion at the bomb’s position.
-    	 * 2. Disables the mesh renderer, making the bomb invisible.
-    	 * 3. Disables the collider, allowing players to move through and walk into an explosion.
-    	 * 4. Destroys the bomb after 0.3 seconds; this ensures all explosions will spawn before the GameObject is destroyed.
-		 */
+	
+	private void Explode() {
+	/*
+	 * 1. Spawns an explosion at the bomb’s position.
+     * 2. Disables the mesh renderer, making the bomb invisible.
+     * 3. Disables the collider, allowing players to move through and walk into an explosion.
+     * 4. Destroys the bomb after 0.3 seconds; this ensures all explosions will spawn before the GameObject is destroyed.
+	 */
 		Instantiate(explosionPrefab, transform.position, Quaternion.identity); //1
-        
-        			GetComponent<MeshRenderer>().enabled = false; //2
-        			transform.Find("Collider").gameObject.SetActive(false); //3
-        			Destroy(gameObject, .5f); //4
+		GetComponent<MeshRenderer>().enabled = false; //2
+		transform.Find("Collider").gameObject.SetActive(false); //3
+		Destroy(gameObject, .3f); //4
 	}
 }
+
