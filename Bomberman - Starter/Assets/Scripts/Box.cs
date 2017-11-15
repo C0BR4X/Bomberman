2ea30a76838436184124b3bf515b6b5b23482750
Julien Del Don <julien.deldon@gmail.com>
2017-11-15 21:18:06 +0100

    working on destructable box
    hitbox of box is not centered :angry: don't really get why...the .mat and texture I got them online. At least we have something, but I am not able to make them explode yet

---
+++ Bomberman - Starter/Assets/Scripts/Box.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+	private bool exploded = false; // Is box exploded
+	private Rigidbody rigidBody;
+	private Transform myTransform;
+	
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void OnTriggerEnter(Collider other) {
+        
+		/*
+		 * 1. Sets the exploded variable
+		 * 2. Destroys the player GameObject.
+		 */
+		if (other.CompareTag("Explosion")) {
+			exploded = true; // 1
+			Destroy(gameObject); // 2
+		}
+	}
+	
+}
